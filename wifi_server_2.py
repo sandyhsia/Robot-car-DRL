a444d7e7c7de74b40b794e1f34ae4f56bae48448
sandyhsia <sandyhsia@sjtu.edu.cn>
2017-02-15 18:59:52 +0800

    server + wifi

---
+++ wifi_server_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+import socket   #socket模块
+
+
+HOST = ''               # Symbolic name meaning all available interfaces
+PORT = 5678             # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Connected by', addr
+while 1:
+    data = conn.recv(1024)
+    if not data: break
+    conn.sendall(data)
+    print data
+conn.close()
